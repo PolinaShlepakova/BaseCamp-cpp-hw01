e7f6b509fe4bb5dc5d93df3d10cf515fa26dc707
Polina Shlepakova <polinashlepakova@gmail.com>
2018-12-02 13:10:10 +0200

    Add test for Move::reset
    Signed-off-by: Polina Shlepakova <polinashlepakova@gmail.com>

--- move/src/main.cpp
+++ move/src/main.cpp
@@ -6,11 +6,15 @@ int main() {
 	Move m2(2, 8);
 	Move m12 = m1.add(m2);
 
-	std::cout << "m1: " << std::endl;
+	std::cout << "m1(5): " << std::endl;
 	m1.showmove();
-	std::cout << "m2: " << std::endl;
+	std::cout << "m2(2, 8): " << std::endl;
 	m2.showmove();
 	std::cout << "m1 + m2: " << std::endl;
+	m12.showmove();
+	
+	m12.reset(1, 11);
+	std::cout << "m12 reset to 1, 11: " << std::endl;
 	m12.showmove();
 	
 	return 0;
